8dfeea16aaa24292c5a7e3749e9d05246f1fe027
AraB-Blue <abediser@gmail.com>
2025-12-27 12:00:36 +0100

    pruebas_escenas

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public static PlayerScript instance;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (instance !=null)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
