679d1b4f9d535058fbd0f9a440b768ad6987534a
AraB-Blue <abediser@gmail.com>
2026-01-26 10:28:24 +0100

    codigoBarraVida

---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    [SerializeField] private PlayerHealth _playerHealth;
+
+    [SerializeField] RectTransform _barRect;
+
+    [SerializeField] RectMask2D _mask;
+
+    private float _maxRightMask;
+    private float _initialRightMask;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // x = left, w = top, y = bottom, z = right
+        _maxRightMask = _barRect.rect.width - _mask.padding.x - _mask.padding.z;
+        _initialRightMask = _mask.padding.z;
+
+    }
+
+   
+    public void SetValue (int newValue)
+    {
+        var targetWidth = newValue * _maxRightMask / _playerHealth.GetMaxHealth();
+        var newRightMask = _maxRightMask + _initialRightMask - targetWidth;
+        var padding = _mask.padding;
+        padding.z = newRightMask;
+        _mask.padding = padding;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
