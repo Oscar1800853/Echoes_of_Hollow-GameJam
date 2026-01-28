2734449a03ce6fb87ab61f1244462cd416573346
AraB-Blue <abediser@gmail.com>
2026-01-28 13:54:23 +0100

    pociones_vida

--- Assets/Scripts/LevelDoorManager.cs
+++ Assets/Scripts/LevelDoorManager.cs
@@ -1,35 +1,68 @@
+using System.Collections;
 using UnityEngine;
 
 public class LevelDoorManager : MonoBehaviour
 {
+    [Header ("Puertas")]
     public Animator[] doorAnimators;
+
+    [Header("Poción de vida")]
+    public GameObject healthPotion;
     private int enemyCount;
-    private bool doorsOpened = false;
+    private bool levelCleared = false;
 
     private void Start()
     {
         enemyCount = GameObject.FindGameObjectsWithTag("enemy").Length;
+
+        if (healthPotion !=null)
+        {
+            healthPotion.SetActive(false); //esta oculta al principio
+        }
     }
 
     public void EnemyDefeated()
     {
-        if (doorsOpened) return;
+        if (levelCleared) return;
 
         enemyCount--;
 
         if(enemyCount <=0)
         {
-            OpenAllDoors();
+            LevelCompleted();
         }
     }
 
-    void OpenAllDoors()
+    void LevelCompleted()
     {
-        doorsOpened = true;
+        levelCleared = true;
+
+        //puertas
 
         foreach (Animator anim in doorAnimators)
         {
             anim.SetTrigger("OpenDoor");
         }
+
+        //pocion
+
+        if (healthPotion !=null)
+        {
+            StartCoroutine(spawnPotion());
+            
+        }
     }
+
+    private IEnumerator spawnPotion()
+    {
+        yield return new WaitForSeconds(2);
+        
+        healthPotion.SetActive(true);
+
+
+    }
+
+
+   
+
 }
